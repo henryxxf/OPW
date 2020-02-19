8e6438dfa734eb7e7747eb469840d67a42df5d2d
henryxxf <henryxxf@163.com>
2020-02-19 15:37:51 +0800

    修改默认值

--- diy.sh
+++ diy.sh
@@ -6,4 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.1.248/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.248/g' package/base-files/files/bin/config_generate
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
